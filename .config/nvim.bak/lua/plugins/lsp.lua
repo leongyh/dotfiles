2bfe9a3fd8f3188d882f3ee055f56d9fd29f1bae
Hung Leong <xblitzerx@gmail.com>
2025-04-14 23:55:58 -0400

    sync:25-04-14_Apr-04-1744689358

---
+++ .config/nvim.bak/lua/plugins/lsp.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonUpdate" },
+    opts = function()
+      return require "configs.mason"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    event = "User FilePost",
+    config = function()
+      require("configs.lspconfig").defaults()
+    end,
+  },
+}
---
+++ .config/nvim/lua/community.lua
@@ -0,0 +1,29 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  -- community colorschemes
+  { import = "astrocommunity.colorscheme.tokyodark-nvim" },
+
+  -- community packs
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cue" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rego" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.xml" },
+  { import = "astrocommunity.pack.yaml" },
+}
